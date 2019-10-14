3948d916470dbcbd36b65d163e9fe0d713555541
Rohit Kumawat <rkumawat125@gmail.com>
2019-10-14 14:36:51 +0530

    google analytics added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Main from './Components/Container/Main';
 
+import ReactGA from 'react-ga';
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    ReactGA.initialize('UA-149928935-1');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }
+
+  someTrackingHandler = () => {
+    ReactGA.event({
+      category: "some category",
+      action: "some action"
+    });
+  }
+
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter> 
         <div>
+          <button onClick={this.someTrackingHandler}>Trigger</button>
           <Main />
         </div>
       </BrowserRouter>
